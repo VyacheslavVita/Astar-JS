35a69d46bdba151e065570822f069a0fcacb22ed
Vyacheslav Vita <vitavyacheslav@gmail.com>
2024-05-01 23:41:43 +0500

    Пофиксил алгоритм
    Добавил расчет из клетки родителя. Теперь алгоритм работает хорошо.

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 var currentStatus = "empty";
 var currentSize;
-
 var startElement;
 var finishElement;
-
 function generateField() {
     currentSize = document.getElementById("size").value;
     var astarPlace = document.querySelector(".astar-place");
-
     for (let i = 0; i < currentSize; i++) {
         let line = document.createElement("div");
         line.classList.add("line");
@@ -19,14 +16,11 @@ function generateField() {
         }
         astarPlace.appendChild(line);
     }
-
     addClickHandlerToCeils();
 }
-
 function swapStatus(statusValue) {
     currentStatus = statusValue;
 }
-
 function addClickHandlerToCeils() {
     document.querySelectorAll(".ceil").forEach(function(ceil) {
         ceil.addEventListener("click", function() {
@@ -37,7 +31,6 @@ function addClickHandlerToCeils() {
         });
     });
 }
-
 class Ceil {
     constructor(status, x, y, g, h, f) {
         this.status = status;
@@ -50,13 +43,10 @@ class Ceil {
         this.isWay = false;
     }
 }
-
 function createCeilsArray() {
     var array = [];
-
     var i = 0;
     var j = 0;
-
     document.querySelectorAll(".ceil").forEach(function(ceil) {
         var obj = new Ceil(ceil.classList[1], i++, j, 0, 0, 0);
         array.push(obj);
@@ -65,16 +55,12 @@ function createCeilsArray() {
             j++;
         }
     });
-
     return array;
 }
-
 function astarSearch() {
     var array = createCeilsArray();
-    
     var closedList = [];
     var openList = [];
-
     array.forEach(element => {
         if (element.status == "start") {
             startElement = element;
@@ -83,45 +69,37 @@ function astarSearch() {
             finishElement = element;
         }
     });
-
     openList.push(startElement);
-
     while (openList.length > 0) {
-        console.log("итерация!")
+        console.log(array);
         var currentElement = openList[0];
-        // console.log(currentElement);
         openList.forEach(element => {
             if (element.f < currentElement.f) {
                 currentElement = element;
             }
         });
-
         let index = openList.find(elem => elem.x == currentElement.x && elem.y == currentElement.y);
         openList.splice(index, 1);
         closedList.push(currentElement);
-
-        // console.log(closedList);
-
         if (currentElement.status == "finish") {
             alert("Путь найден!");
             break;
         }
-
         array.forEach(element => {
             if (Math.abs(currentElement.x - element.x) <= 1 && Math.abs(currentElement.y - element.y) <= 1) {
-                // console.log(element);
                 let check = closedList.find(elem => elem == element);
                 if (check == undefined) {
-                    // console.log(element);
                     if (element.status != "wall") {
-                        let tempF = getDistance(element, currentElement) + getHeuristic(element);
+                        if (element.parent == null) {
+                            element.parent = currentElement;
+                        }
+                        let tempF = getDistance(element, currentElement) + getHeuristic(element) + element.parent.g;
                         if (element.f == 0 || tempF < element.f) {
-                            element.g = getDistance(element, currentElement);
+                            element.g = getDistance(element, currentElement) + element.parent.g;
                             element.h = getHeuristic(element);
                             element.f = element.g + element.h;
                             element.parent = currentElement;
                         }
-
                         let check = openList.find(elem => elem == element);
                         if (check == undefined) {
                             openList.push(element);
@@ -131,16 +109,11 @@ function astarSearch() {
             }
         });
     }
-
     let currentCeil = finishElement;
     while (currentCeil != null) {
         currentCeil.isWay = true;
-        // console.log(currentCeil);
         currentCeil = currentCeil.parent;
     }
-    
-    console.log(array);
-
     let arr = document.querySelectorAll(".ceil");
     console.log(arr);
     for (let i = 0; i < currentSize * currentSize; i++) {
@@ -151,9 +124,9 @@ function astarSearch() {
 }
 
 function getDistance(ceil1, ceil2) {
-    return Math.sqrt(Math.pow(ceil1.x - ceil2.x, 2) + Math.pow(ceil1.y - ceil2.y, 2));
+    return Math.trunc(Math.sqrt(Math.pow(ceil1.x - ceil2.x, 2) + Math.pow(ceil1.y - ceil2.y, 2))) * 10;
 }
 
 function getHeuristic(ceil) {
-    return Math.abs(ceil.x - finishElement.x);
+    return Math.abs(ceil.x - finishElement.x) * 10;
 }
